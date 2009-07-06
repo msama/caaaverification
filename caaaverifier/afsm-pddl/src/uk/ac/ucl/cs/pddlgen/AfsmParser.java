7c37a147c0ed4b509dd314da30c0eb79e5c2558b
michele.sama <michele.sama@8b6c40dc-6253-11de-a615-ff9c373c20b3>
2009-07-06 17:28:36 +0000

    Extended AfsmParser still buggy

--- caaaverifier/afsm-pddl/src/uk/ac/ucl/cs/pddlgen/AfsmParser.java
+++ caaaverifier/afsm-pddl/src/uk/ac/ucl/cs/pddlgen/AfsmParser.java
@@ -10,13 +10,23 @@ import phoneAdapter.PhoneAdapterAfsm;
 import com.sun.org.apache.bcel.internal.generic.ATHROW;
 
 import uk.ac.ucl.cs.afsm.common.AdaptationFiniteStateMachine;
+import uk.ac.ucl.cs.afsm.common.Assignment;
+import uk.ac.ucl.cs.afsm.common.Rule;
 import uk.ac.ucl.cs.afsm.common.State;
+import uk.ac.ucl.cs.afsm.common.predicate.Constant;
+import uk.ac.ucl.cs.afsm.common.predicate.Operator;
+import uk.ac.ucl.cs.pddlgen.ebnf.ActionDef;
+import uk.ac.ucl.cs.pddlgen.ebnf.ActionDefBody;
+import uk.ac.ucl.cs.pddlgen.ebnf.ActionFunctor;
+import uk.ac.ucl.cs.pddlgen.ebnf.AtomicFormula;
 import uk.ac.ucl.cs.pddlgen.ebnf.AtomicFormulaSkeleton;
 import uk.ac.ucl.cs.pddlgen.ebnf.ConstantsDef;
 import uk.ac.ucl.cs.pddlgen.ebnf.Domain;
 import uk.ac.ucl.cs.pddlgen.ebnf.DomainVarDeclaration;
 import uk.ac.ucl.cs.pddlgen.ebnf.DomainVarsDef;
+import uk.ac.ucl.cs.pddlgen.ebnf.Effect;
 import uk.ac.ucl.cs.pddlgen.ebnf.ExtensionDef;
+import uk.ac.ucl.cs.pddlgen.ebnf.GD;
 import uk.ac.ucl.cs.pddlgen.ebnf.Literal;
 import uk.ac.ucl.cs.pddlgen.ebnf.MethodDef;
 import uk.ac.ucl.cs.pddlgen.ebnf.Name;
@@ -26,6 +36,7 @@ import uk.ac.ucl.cs.pddlgen.ebnf.RequireDef;
 import uk.ac.ucl.cs.pddlgen.ebnf.RequireKey;
 import uk.ac.ucl.cs.pddlgen.ebnf.SafetyDef;
 import uk.ac.ucl.cs.pddlgen.ebnf.StructureDef;
+import uk.ac.ucl.cs.pddlgen.ebnf.Term;
 import uk.ac.ucl.cs.pddlgen.ebnf.TimelessDef;
 import uk.ac.ucl.cs.pddlgen.ebnf.TypedList;
 import uk.ac.ucl.cs.pddlgen.ebnf.TypesDef;
@@ -39,7 +50,7 @@ public class AfsmParser {
 	
 	private Map<String, Variable> variables = new HashMap<String, Variable>();
 	
-	private Map<String, AtomicFormulaSkeleton> predicates = new HashMap<String, AtomicFormulaSkeleton>();
+	private Map<String, Predicate> predicates = new HashMap<String, Predicate>();
 	
 	/**
 	 * @param afsm
@@ -144,7 +155,7 @@ public class AfsmParser {
 			TypedList<Variable> typedList = TypedList.create(vars);
 			AtomicFormulaSkeleton skeleton = AtomicFormulaSkeleton.create(predicate, typedList);
 			formulas.add(skeleton);
-			predicates.put(key, skeleton);
+			predicates.put(key, predicate);
 		}
 		
 		for (String name : afsm.variables.keySet()) {
@@ -157,7 +168,7 @@ public class AfsmParser {
 			TypedList<Variable> typedList = TypedList.create(vars);
 			AtomicFormulaSkeleton skeleton = AtomicFormulaSkeleton.create(predicate, typedList);
 			formulas.add(skeleton);
-			predicates.put(key, skeleton);
+			predicates.put(key, predicate);
 		}
 		
 		return PredicatesDef.create(formulas);
@@ -182,9 +193,77 @@ public class AfsmParser {
 	 * @return
 	 */
 	private List<StructureDef> createStructureDefs() {
-		//MethodDef methodDef = MethodDef.create();
+		//ActionDef actionDef = ActionDef.create();
 		//StructureDef.create(methodDef);
+		for (Rule rule : afsm.rules) {
+			ActionFunctor functor = new ActionFunctor(Name.create("rule_" + rule.getName()));
+			
+			List<Variable> varName = new ArrayList<Variable>();
+			for (uk.ac.ucl.cs.afsm.common.predicate.Variable v : rule.getAllVariables()) {
+				varName.add(variables.get(v.getName()));
+			}
+			// Add state as a variable
+			varName.add(variables.get("state"));
+			TypedList<Variable> vars = TypedList.create(varName);
+			
+			GD preconditions = parsePredicate(rule.getTrigger());
+			
+			Effect[] and = new Effect[rule.action.size()];
+			int i = 0;
+			for (Assignment a : rule.action) {
+				Effect eff;
+				// predicate must be one satisfying the variable
+				Predicate predicate = predicates.get(a.getVariable().getName());
+				// the term is the name of the local variable
+				Term t = Term.create(names.get(a.getVariable().getName()));
+				AtomicFormula<Term> formula = AtomicFormula.create(predicate, t);
+				if (a instanceof Assignment.Satisfy) {
+					eff = Effect.createFormula(formula);
+				} else {
+					eff = Effect.createNot(formula);
+				}
+				and[i++] = eff;
+			}
+			Effect effect = Effect.createAnd(and);
+			
+			
+			ActionDefBody body = ActionDefBody.create(preconditions, effect);
+			ActionDef actionDef = ActionDef.create(functor, vars, body);
+		}
+		
 		List<StructureDef> defs = new ArrayList<StructureDef>();
 		return defs;
+	}
+	
+	private GD parsePredicate(uk.ac.ucl.cs.afsm.common.predicate.Predicate predicate) {
+		if (predicate == Constant.TRUE) {
+			throw new IllegalStateException("True not defined: " + predicate);
+		} else if (predicate == Constant.FALSE) {
+			throw new IllegalStateException("False not defined: " + predicate);
+		} if (predicate instanceof uk.ac.ucl.cs.afsm.common.predicate.Variable) {
+			uk.ac.ucl.cs.afsm.common.predicate.Variable v = (uk.ac.ucl.cs.afsm.common.predicate.Variable)predicate;
+			Term t = Term.create(names.get(v.getName()));
+			AtomicFormula<Term> formula = AtomicFormula.create(predicates.get(v.getName()), t);
+			return GD.createFormula(formula);
+		} else if (predicate instanceof Operator.Not) {
+			Operator.Not not = (Operator.Not) predicate;
+			return GD.createNot(parsePredicate(not.getPredicate()));
+		} else if (predicate instanceof Operator.And) {
+			Operator.And and = (Operator.And) predicate;
+			GD[] andGd = new GD[and.size()];
+			for (int i = 0; i < and.size(); i++) {
+				andGd[i] = parsePredicate(and.get(i));
+			}
+			return GD.createAnd(andGd);
+		} else if (predicate instanceof Operator.Or) {
+			Operator.Or or = (Operator.Or) predicate;
+			GD[] orGd = new GD[or.size()];
+			for (int i = 0; i < or.size(); i++) {
+				orGd[i] = parsePredicate(or.get(i));
+			}
+			return GD.createOr(orGd);
+		}
+		throw new IllegalStateException(
+				"This method should have already returned a value: " + predicate);
 	}
 }
--- caaaverifier/afsm-pddl/src/uk/ac/ucl/cs/pddlgen/ebnf/ActionFunctor.java
+++ caaaverifier/afsm-pddl/src/uk/ac/ucl/cs/pddlgen/ebnf/ActionFunctor.java
@@ -9,13 +9,22 @@ package uk.ac.ucl.cs.pddlgen.ebnf;
  */
 public class ActionFunctor extends Streamable {
 
+	private Name name;
+	
+	/**
+	 * @param name
+	 */
+	public ActionFunctor(Name name) {
+		this.name = name;
+	}
+	
 	/* (non-Javadoc)
 	 * @see uk.ac.ucl.cs.pddlgen.ebnf.Streamable#printInternal()
 	 */
 	@Override
 	protected void printInternal() {
-		// TODO Auto-generated method stub
-
+		writeInto(name);
 	}
 
+	
 }
