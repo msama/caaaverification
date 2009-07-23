bdf6a156e3acb8e7a2a8a357ce4854f3e779b7b1
michele.sama <michele.sama@8b6c40dc-6253-11de-a615-ff9c373c20b3>
2009-07-23 13:46:27 +0000

    fixed constraints

--- caaaverifier/afsm-pddl/src/uk/ac/ucl/cs/pddlgen/ebnf/RequireKey.java
+++ caaaverifier/afsm-pddl/src/uk/ac/ucl/cs/pddlgen/ebnf/RequireKey.java
@@ -16,18 +16,30 @@ public class RequireKey extends Streamable {
 		this.label = label;
 	}
 
+	public static final RequireKey STRIPS = new RequireKey("strips");
 	public static final RequireKey TYPING = new RequireKey("typing");
+	public static final RequireKey DISJUNCTIVE_PRECONDITIONS = new RequireKey("disjunctive-preconditions");
+	public static final RequireKey EQUALITY = new RequireKey("equality");
+	public static final RequireKey EXISTENTIAL_PRECONDITIONS = new RequireKey("existential-preconditions");
+	public static final RequireKey UNIVERSAL_PRECONDITIONS = new RequireKey("universal-preconditions");
+	public static final RequireKey QUANTIFIED_PRECONDITIONS = new RequireKey("quantified-preconditions");
+	public static final RequireKey CONDITIONAL_EFFECTS = new RequireKey("conditional-effects");
+	public static final RequireKey ACTION_EXPANSIONS = new RequireKey("action-expasions");
+	public static final RequireKey FOREACH_EXPANSIONS = new RequireKey("foreach-expansions");
+	public static final RequireKey DAG_EXPANSIONS = new RequireKey("dag-expansion");
+	public static final RequireKey DOMAIN_AXIOMS = new RequireKey("domain-axioms");
+	public static final RequireKey SUBGOAL_THROUGH_AXIOMS = new RequireKey("subgoal-through-axioms");
 	public static final RequireKey SAFETY_CONSTRAINTS = new RequireKey("safety-constraints");
 	public static final RequireKey EXPRESSION_EVALUATION = new RequireKey("expression-evaluation");
-	public static final RequireKey DOMAIN_AXIOMS = new RequireKey("domain-axioms");
-	public static final RequireKey ACTION_EXPANSIONS = new RequireKey("action-expasions");
 	public static final RequireKey FLUENTS = new RequireKey("fluents");
-	public static final RequireKey EXISTENTIAL_PRECONDITIONS = new RequireKey("existential-preconditions");
-	public static final RequireKey CONDITIONAL_EFFECTS = new RequireKey("conditional-effects");
-	public static final RequireKey UNIVERSAL_PRECONDITIONS = new RequireKey("universal-preconditions");
-	public static final RequireKey DISJUNCTIVE_PRECONDITIONS = new RequireKey("disjunctive-preconditions");
-	public static final RequireKey FOREACH_EXPANSION = new RequireKey("foreach-expansion");
+	public static final RequireKey OPEN_WORLD = new RequireKey("open-world");
+	public static final RequireKey TRUE_NEGATION = new RequireKey("true-negation");
+	public static final RequireKey ADL = new RequireKey("adl");
+	public static final RequireKey UCPOP = new RequireKey("ucpop");
 
+
+		
+	
 	private final String label;
 	
 	/* (non-Javadoc)
