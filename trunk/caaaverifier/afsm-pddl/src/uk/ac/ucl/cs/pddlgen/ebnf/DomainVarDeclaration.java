c28ee116906f6200834ec0508ef9f9e50bb25549
michele.sama <michele.sama@8b6c40dc-6253-11de-a615-ff9c373c20b3>
2009-07-29 12:38:24 +0000

    implemented domain var declaration

--- trunk/caaaverifier/afsm-pddl/src/uk/ac/ucl/cs/pddlgen/ebnf/DomainVarDeclaration.java
+++ trunk/caaaverifier/afsm-pddl/src/uk/ac/ucl/cs/pddlgen/ebnf/DomainVarDeclaration.java
@@ -9,13 +9,27 @@ package uk.ac.ucl.cs.pddlgen.ebnf;
  */
 public class DomainVarDeclaration extends Streamable {
 
+	private Name name;
+	
+	/**
+	 * @param name
+	 */
+	private DomainVarDeclaration(Name name) {
+		this.name = name;
+	}
+
 	/* (non-Javadoc)
 	 * @see uk.ac.ucl.cs.pddlgen.ebnf.Streamable#printInternal()
 	 */
 	@Override
 	protected void printInternal() {
-		// TODO Auto-generated method stub
-
+		writeInto(pw, name);
 	}
 
+	public static DomainVarDeclaration create(Name name) {
+		if (name == null) {
+			throw new IllegalArgumentException("Variable name cannot be null.");
+		}
+		return new DomainVarDeclaration(name);
+	}
 }
